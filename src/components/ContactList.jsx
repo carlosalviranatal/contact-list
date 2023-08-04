2efdefde8edbdc840f1a5d4f42b7844d9f9bf773
ChuangStephanie <stephanie231951@yahoo.com>
2023-08-03 20:36:22 -0500

    added contactRow(wip)

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import ContactRow from "./ContactRow";
 
 const dummyContacts = [
     { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
@@ -25,7 +26,7 @@ function ContactList() {
                     <td>Number</td>
                 </tr>
                 <tr>
-                    {
+                    { 
                         
                     }
                 </tr>
