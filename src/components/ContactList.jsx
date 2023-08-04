cc54d5bd433972323be7b00dee8e4259423b8b34
carlosalviranatal <carlosalviranatal@DESKTOP-A4A091C.localdomain>
2023-08-03 19:52:34 -0500

    Set up Table

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function ContactList() {
+
+    return (
+        <table>
+            <thead>
+                <tr colSpan="3">Contact List</tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Name</td>
+                    <td>Email</td>
+                    <td>Number</td>
+                </tr>
+                <tr>
+                    {
+                        
+                    }
+                </tr>
+            </tbody>
+        </table>
+    );
+}
+
+export default ContactList
